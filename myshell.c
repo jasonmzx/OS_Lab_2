6b1e49985d0f2e8ca099f033ae3cd85d7272c55d
jasonmzx <jasonmvocet@gmail.com>
2024-01-27 01:10:39 -0500

    r1.ix

--- myshell.c
+++ myshell.c
@@ -8,12 +8,35 @@
 #define MAX_INPUT_LENGTH 512
 #define MAX_PATH_LENGTH 1024
 
+void set_shell_env_variable() { //* Requirement 1. IX
+    char execpath[MAX_PATH_LENGTH];
+
+    // Read the symbolic link '/proc/self/exe' to get the path of the current executable
+    // Using ssize_t as the readlink can return -1 (negative) therefore signed int is needd
+    
+    ssize_t len = readlink("/proc/self/exe", execpath, sizeof(execpath) - 1);
+    
+    if (len != -1) {
+        execpath[len] = '\0'; // Null-terminate the path
+        
+        if (setenv("shell", execpath, 1) != 0) { // Set the 'shell' environment variable
+            perror("setenv failure !!");
+        } else {
+            printf("\nShell environment variable set to: %s\n", execpath);
+        }
+    } else {
+        perror("readlink failure !!");
+    }
+}
+
 int main(int argc, char *argv[]) {
+    
+    set_shell_env_variable();
 
     char* SHELL_PREFIX = "> myshell";
     char* SHELL_SEPERATOR = "$"; //* Example: myshell/path/to/dir $ <user input>
 
-    //* ---- Visual Appeal (additional) Escape Codes ----
+    //* ---- Visual Appeal (additional features) Escape Codes ----
 
     char* GREEN_EC =  "\033[1;32m";
     char* YELLOW_EC = "\033[1;33m";
@@ -22,11 +45,10 @@ int main(int argc, char *argv[]) {
     //* ---- Main Shell Buffers ----
 
     char input[MAX_INPUT_LENGTH];
-    char cwd[MAX_PATH_LENGTH];
+    char cwd[MAX_PATH_LENGTH]; //Current Path myshell is in (usually starts at shell environ. loc)
     char full_prompt[MAX_INPUT_LENGTH + MAX_PATH_LENGTH];
 
-
-    printf("\nWelcome to myshell, By: Jason Manarroo 100825106 \n\n");
+    printf("Welcome to myshell, By: Jason Manarroo 100825106 \n\n");
 
     // Main loop
     while (1) {
